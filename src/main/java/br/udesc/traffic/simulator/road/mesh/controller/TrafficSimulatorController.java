991fe57f2e5f144b18087fddcf0b0d1bac73bec1
Mateus-Gabardo <92892326+Mateus-Gabardo@users.noreply.github.com>
2023-05-23 22:06:13 -0300

    Feito método para retornar elementos de renderização

--- src/main/java/br/udesc/traffic/simulator/road/mesh/controller/TrafficSimulatorController.java
+++ src/main/java/br/udesc/traffic/simulator/road/mesh/controller/TrafficSimulatorController.java
@@ -3,13 +3,19 @@ package br.udesc.traffic.simulator.road.mesh.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.udesc.traffic.simulator.road.mesh.model.GlobalContants;
+import br.udesc.traffic.simulator.road.mesh.model.road.PieceModel;
+import br.udesc.traffic.simulator.road.mesh.model.road.RoadDown;
+import br.udesc.traffic.simulator.road.mesh.model.road.RoadLeft;
+import br.udesc.traffic.simulator.road.mesh.model.road.RoadRight;
+import br.udesc.traffic.simulator.road.mesh.model.road.RoadUp;
 import br.udesc.traffic.simulator.road.mesh.observer.ObserverView;
 import br.udesc.traffic.simulator.road.mesh.singleton.MeshRepository;
 
-public class TrafficSimulatorController implements AbstractTrafficSimulatorTableController, AbstractController{
+public class TrafficSimulatorController implements AbstractTrafficSimulatorTableController, AbstractController {
 	List<ObserverView> observers;
 	private int[][] roadMesh;
-		
+
 	public TrafficSimulatorController() {
 		super();
 		observers = new ArrayList<>();
@@ -27,29 +33,59 @@ public class TrafficSimulatorController implements AbstractTrafficSimulatorTable
 	}
 
 	@Override
-	public Object getValueAt(int rowIndex, int columnIndex) {
-		// TODO Auto-generated method stub
-		return null;
+	public PieceModel getValueAt(int rowIndex, int columnIndex) {
+		int tipo = roadMesh[rowIndex][columnIndex];
+		switch (tipo) {
+		case GlobalContants.UP: {
+			return new RoadUp(tipo);
+		}
+		case GlobalContants.RIGHT: {
+			return new RoadRight(tipo);
+		}
+		case GlobalContants.DOWN: {
+			return new RoadDown(tipo);
+		}
+		case GlobalContants.LEFT: {
+			return new RoadLeft(tipo);
+		}
+		default:
+			return null;
+		}
 	}
 
 	@Override
 	public void addObserver(ObserverView view) {
-		observers.add(view);		
+		observers.add(view);
 	}
 
 	@Override
 	public void removeObserver(ObserverView view) {
 		observers.remove(view);
-		
+
 	}
 
 	@Override
 	public void notifyObserver() {
-		for(ObserverView view : observers) {
+		for (ObserverView view : observers) {
 			// chama algum metodo que deve ser implementado na view
 			// atualizar a tela por exemplo
 		}
-		
+
+	}
+
+	/**
+	 * Avalia se vai retornar algum valor ou não
+	 * 
+	 * @param linha
+	 * @param coluna
+	 * @return
+	 */
+	public boolean hasElementAt(int linha, int coluna, int[][] state) {
+		if (state[linha][coluna] == 0) {
+			return false;
+		} else {
+			return true;
+		}
 	}
 
 }
---
+++ src/main/java/br/udesc/traffic/simulator/road/mesh/model/GlobalContants.java
@@ -0,0 +1,18 @@
+package br.udesc.traffic.simulator.road.mesh.model;
+
+public class GlobalContants {
+	
+	public static final int UP = 1;
+	public static final int RIGHT = 2;
+	public static final int DOWN = 3;
+	public static final int LEFT = 4;
+	public static final int CRUZAMENTO_UP= 5;
+	public static final int CRUZAMENTO_RIGHT = 6;
+	public static final int CRUZAMENTO_DOWN = 7;
+	public static final int CRUZAMENTO_LEFT = 8;
+	public static final int CRUZAMENTO_UP_RIGHT = 9;
+	public static final int CRUZAMENTO_UP_LEFT = 10;
+	public static final int CRUZAMENTO_RIGHT_DOWN = 11;
+	public static final int CRUZAMENTO_LEFT_DOWN = 12;
+
+}
